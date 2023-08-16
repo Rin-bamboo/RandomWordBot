132fb99788d4f0b439f51c0c2715c7e63f20f5f8
bamboo <78258130+Rin-bamboo@users.noreply.github.com>
2023-08-16 21:59:29 +0900

    修正:ログ処理の追加
    テキスト読み込み処理にログ出力を追加

--- text_input.py
+++ text_input.py
@@ -1,12 +1,19 @@
+from log_setting import getLogger
+logger = getLogger(__name__)
+
 
 def inptfile(name):
-    #=========テキストファイルの読み込み============
+    logger.info("=========テキストファイルの読み込み============")
+
     path = 'text/'+ name +'.txt'  #ヘルプファイルの読み込み
     try:
         f = open(path,encoding='utf-8')
+        logger.info(f)
         input_value = f.read()
+        logger.info("読み込みデータ：" + input_value)
         f.close()
-    except:
+    except Exception as ex:
+        logger.warning(f"エラー情報：{ex}")
         input_value = "テキストが読み込めませんでした"
     finally:
         return input_value
